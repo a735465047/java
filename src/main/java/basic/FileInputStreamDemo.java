f0eb0356d1f1681bcdffe16dce8b60869e5280ba
wangqiaohong <wangqiaohong@lianjia.com>
2018-10-28 23:37:39 +0800

    modify

--- src/main/java/basic/FileInputStreamDemo.java
+++ src/main/java/basic/FileInputStreamDemo.java
@@ -1,7 +1,14 @@
 package basic;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class FileInputStreamDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fs=new FileInputStream("/tmp/test");
+        for (int i=0;i<100;i++){
+            System.out.println((char)fs.read());
+        }
 
     }
 }
